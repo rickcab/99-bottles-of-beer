686b0dd57f192a7ce3f874df45923f38cb22c343
Rick Cabrera <rick@findexp.com>
2013-10-08 13:30:44 -0400

    Create 99bottlesOfBeer.groovy

---
+++ 99bottlesOfBeer.groovy
@@ -0,0 +1,51 @@
+@Category(Integer)
+class MondayNightBrewery {
+    static class Beer {}
+    
+    def drink(one, beer) { 
+        this.downto(one, beer)
+    }
+    
+    def bottlesOfBeerOnTheWall(grab) { 
+        println "$this bottles of beer on the wall..."
+        this.drink(1, { grab(it) }) 
+    }
+    
+    def bottlesOfBeer() { 
+        println "$this bottles of beer"  
+        def wall = []
+        this.drink(1, { wall << new Beer() }) 
+        wall 
+    }
+}
+
+List.metaClass.takeOneDown  = { 
+    println "Take One Down"
+    delegate.pop()
+    delegate 
+}
+
+List.metaClass.passItAround = { 
+    println "Pass it around\n${delegate.size()} bottles of beer on the wall\n"
+    delegate ?: 0 
+}
+
+List.metaClass.bottlesOfBeerOnTheWall = {}
+
+use (MondayNightBrewery) {
+    99.bottlesOfBeerOnTheWall { ninetyNine ->
+        ninetyNine
+            .bottlesOfBeer()
+            .takeOneDown()
+            .passItAround()
+            .bottlesOfBeerOnTheWall()
+    }
+    
+    // Crash!
+}
+
+
+
+
+
+
